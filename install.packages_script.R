2935e3cef240d036701cd9a0e683a41dec2c6a85
Rodrigo MontAlegre <rodrigo.montalegre@hotmail.com>
2021-01-18 17:12:00 +0100

    Wrote a script that installs all packages needed for the main script.

---
+++ install.packages_script.R
@@ -0,0 +1,11 @@
+#In order to run the general R script for this analysis, run this script beforhand to ensure that all necessary packaged are installed.
+
+install.packages("data.table")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("magrittr")
+install.packages("ggplot2")
+install.packages("rgdal")
+install.packages("viridis") #for map coloring
+install.packages("outliers") #for testing max value in SeoulFloating
+install.packages("ggthemes") #for the theme_map() theme 
